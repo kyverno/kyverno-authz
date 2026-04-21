bca8b5c8f121968fcc67bb5de4b6d2e72c1e036d
aerosouund <aerosound161@gmail.com>
2026-04-21 22:33:33 +0200

    what
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/engine/sources/composite_source.go
+++ pkg/engine/sources/composite_source.go
@@ -114,7 +114,6 @@ func (s *compositeStore) handlePolex(excKey string, exc *v1.PolicyException, isD
 			polState.exceptions[excKey] = excState
 			polState.exceptionEventCounter++
 			excState.references[polRef.Name] = polState
-			s.exceptions[excKey] = excState
 		}
 	}
 }
