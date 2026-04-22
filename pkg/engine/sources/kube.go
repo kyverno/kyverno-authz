31081a6653ea533da3d29032279b8ffc292cb7a9
aerosouund <aerosound161@gmail.com>
2026-04-22 18:00:30 +0200

    trying out random stuff
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/engine/sources/kube.go
+++ pkg/engine/sources/kube.go
@@ -2,7 +2,6 @@ package sources
 
 import (
 	"context"
-	"fmt"
 
 	v1 "github.com/kyverno/api/api/policies.kyverno.io/v1"
 
@@ -38,19 +37,19 @@ func NewKube[POLICY any](name string, mgr ctrl.Manager, compiler engine.Compiler
 		compositeStore,
 		compositeStore.keyFunc,
 		func(ctx context.Context, _ string, in *v1.ValidatingPolicy) (POLICY, error) {
-			var zero POLICY
+			// var zero POLICY
 			exceptions := []*v1.PolicyException{}
 			// TODO: if we ever choose to support namespaced policy exceptions we would need to key on ns/name
-			polState, ok := compositeStore.policies[in.Name]
-			if !ok {
-				return zero, fmt.Errorf("attempting to fetch and compile a policy that doesn't exist")
-			}
-			for _, exc := range polState.exceptions {
-				if exc.exception.Spec.EvaluationMode != in.Spec.EvaluationMode() {
-					continue
-				}
-				exceptions = append(exceptions, &exc.exception)
-			}
+			// polState, ok := compositeStore.policies[in.Name]
+			// if !ok {
+			// 	return zero, fmt.Errorf("attempting to fetch and compile a policy that doesn't exist")
+			// }
+			// for _, exc := range polState.exceptions {
+			// 	if exc.exception.Spec.EvaluationMode != in.Spec.EvaluationMode() {
+			// 		continue
+			// 	}
+			// 	exceptions = append(exceptions, &exc.exception)
+			// }
 			policy, err := compiler.Compile(in, exceptions)
 			return policy, err.ToAggregate()
 		},
